6bac56a3f4aabd9a96d9d9c02b1fe029cc68e746
TravelEngineers <2375026780@qq.com>
2017-05-24 15:43:15 +0800

    zuul 自定义过滤器

--- SpringBoot/src/main/java/com/zgs/domain/User.java
+++ SpringBoot/src/main/java/com/zgs/domain/User.java
@@ -4,4 +4,7 @@ package com.zgs.domain;
  * Created by Smily on 2017/5/5.
  */
 public class User {
+    private String username;
+    private String password;
+
 }
--- SpringBoot/src/main/java/com/zgs/provider/UserService.java
+++ SpringBoot/src/main/java/com/zgs/provider/UserService.java
@@ -1,7 +1,27 @@
 package com.zgs.provider;
 
+import com.zgs.domain.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
 /**
  * Created by Smily on 2017/5/5.
  */
-public class UserService {
+@Mapper
+public interface UserService {
+    @Insert("insert into account(name, money) values(#{name}, #{money})")
+    int add(@Param("name") String name, @Param("money") double money);
+
+    @Update("update account set name = #{name}, money = #{money} where id = #{id}")
+    int update(@Param("name") String name, @Param("money") double money, @Param("id") int  id);
+
+    @Delete("delete from account where id = #{id}")
+    int delete(int id);
+
+    @Select("select id, name as name, money as money from account where id = #{id}")
+    User findAccount(@Param("id") int id);
+
+    @Select("select id, name as name, money as money from account")
+    List<User> findAccountList();
 }
