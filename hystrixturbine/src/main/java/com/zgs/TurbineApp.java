85406c0dc8df21d7bc350fc053e93cc93d135482
TravelEngineers <2375026780@qq.com>
2017-05-26 21:35:23 +0800

    turbine 更新

--- hystrixturbine/src/main/java/com/zgs/TurbineApp.java
+++ hystrixturbine/src/main/java/com/zgs/TurbineApp.java
@@ -1,7 +1,24 @@
 package com.zgs;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
 /**
  * Created by Shmily on 2017/5/26.
  */
+@SpringBootApplication
+@EnableEurekaClient //启动EnableEureka客户端
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableTurbine
 public class TurbineApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApp.class, args);
+    }
 }
