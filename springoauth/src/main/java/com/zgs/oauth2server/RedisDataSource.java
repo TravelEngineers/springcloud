a6ac45f5cc48ef4c70ea724fb847f975ebed2591
TravelEngineers <2375026780@qq.com>
2017-05-31 21:14:18 +0800

    1、更新数据源相关基础信息 2、添加springboot打包依赖

--- springoauth/src/main/java/com/zgs/oauth2server/RedisDataSource.java
+++ springoauth/src/main/java/com/zgs/oauth2server/RedisDataSource.java
@@ -1,7 +1,14 @@
 package com.zgs.oauth2server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by Shmily on 2017/5/30.
  */
+@Repository
 public class RedisDataSource {
+    @Autowired
+    private StringRedisTemplate template;
 }
