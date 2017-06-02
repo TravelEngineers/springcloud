5ad6eca5aa6791de997eba4e0a6f6daa1aaa27dd
TravelEngineers <2375026780@qq.com>
2017-06-02 15:47:27 +0800

    redis集成处理逻辑

--- springoauth/src/main/java/com/zgs/service/ClinetcredentTokenService.java
+++ springoauth/src/main/java/com/zgs/service/ClinetcredentTokenService.java
@@ -1,16 +1,45 @@
 package com.zgs.service;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Created by Shmily on 2017/5/31.
+ *  客户端凭证方式获取授权token
+ *  Created by Shmily on 2017/5/31.
  */
 @Component
 public class ClinetcredentTokenService {
+    @Autowired
+    private StringRedisTemplate  redisTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     public JSONObject getToken(HttpServletRequest request){
-        return null;
+        JSONObject respData=new JSONObject();
+        String client_id=request.getParameter("client_id");
+        String client_secret=request.getParameter("client_secret");
+        if(checkInfo(client_id,client_secret)){
+             redisTemplate.opsForValue().set("client_id","client_secret");
+            //生成调用方授权信息
+        }else{
+            respData.put("respCode",2001);
+            respData.put("respDesc","调用方身份异常");
+        }
+        return respData;
+    }
+
+    /**
+     * 校验客户端有效性
+     * @param client_id
+     * @param client_secret
+     * @return
+     */
+    private boolean checkInfo(String client_id,String client_secret){
+        return true;
     }
 }
